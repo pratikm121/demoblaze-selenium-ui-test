13a1c448440b6470ff349af12de49f6157466903
Pratik Mehta <pratikm121@gmail.com>
2021-06-19 15:45:29 +0200

    Adding more test cases.

--- src/test/java/com/pratik/ui/test/demoblaze_selenium_ui_test/MainPage.java
+++ src/test/java/com/pratik/ui/test/demoblaze_selenium_ui_test/MainPage.java
@@ -1,10 +1,10 @@
 package com.pratik.ui.test.demoblaze_selenium_ui_test;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage {
 
@@ -14,6 +14,18 @@ public class MainPage {
     public SelenideElement dellLink = $x("//a[contains(text(),'Dell i7 8gb')]");
     public SelenideElement cartLink = $x("//a[@id='cartur']");
     public SelenideElement placeOrder = $x("//button[contains(text(),'Place Order')]");
+    public SelenideElement formNameInput = $x("//input[@id='name']");
+    public SelenideElement formCountryInput = $x("//input[@id='country']");
+    public SelenideElement formCityInput = $x("//input[@id='city']");
+    public SelenideElement formCardInput = $x("//input[@id='card']");
+    public SelenideElement formMonthInput = $x("//input[@id='month']");
+    public SelenideElement formYearInput = $x("//input[@id='year']");
+    public SelenideElement purchaseButton = $x("//button[contains(text(),'Purchase')]");
+    public SelenideElement purchaseTotal = $x("//label[@id='totalm']");
+    public SelenideElement confirmationBoxText = $(".sweet-alert > p");
+    public SelenideElement confirmationBoxOkBtn = $x("//button[contains(text(),'OK')]");
+    public ElementsCollection tableRow =  $$("#tbodyid > tr");
+
     public final String URL= "https://demoblaze.com/";
     public final long WAIT_TIME = 3000;
 
--- src/test/java/com/pratik/ui/test/demoblaze_selenium_ui_test/Testing.java
+++ src/test/java/com/pratik/ui/test/demoblaze_selenium_ui_test/Testing.java
@@ -1,9 +1,11 @@
 package com.pratik.ui.test.demoblaze_selenium_ui_test;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -11,15 +13,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class Testing {
 
-
+    MainPage page = new MainPage();
     @BeforeAll
     public static void setUpAll() {
         Configuration.browserSize = "1920x1080";
     }
 
-    @Test
-    public void test(){
-        MainPage page = new MainPage();
+    @Order(1)
+    @Test()
+    void whenItemIsSelected_ThenItShouldShowInCart(){
         open(page.URL);
         page.laptopsLink.click();
         page.sonyLink.click();
@@ -37,10 +39,37 @@ public class Testing {
 
         page.cartLink.click();
         sleep(page.WAIT_TIME);
-        SelenideElement deleteRow = $$("#tbodyid > tr").find(Condition.text("Dell i7 8gb"));
+        $$("#tbodyid > tr").shouldHave(CollectionCondition.size(2));
+        SelenideElement deleteRow = page.tableRow.find(Condition.text("Dell i7 8gb"));
         deleteRow.findElement(By.linkText("Delete")).click();
         sleep(page.WAIT_TIME);
+        $$("#tbodyid > tr").shouldHave(CollectionCondition.size(1));
+        SelenideElement selectedRow = page.tableRow.find(Condition.text("Sony vaio i5"));
+        selectedRow.shouldBe(Condition.exist);
+
+    }
+
+    @Order(2)
+    @Test()
+    void whenItemIsProcessed_ThenItShouldBePurchased(){
         page.placeOrder.click();
-        sleep(page.WAIT_TIME);
+        page.purchaseTotal.shouldHave(Condition.text("790"));
+        page.formNameInput.setValue("Pratik");
+        page.formCountryInput.setValue("India");
+        page.formCityInput.setValue("Kolkata");
+        page.formCardInput.setValue("21323 454356 456456");
+        page.formMonthInput.setValue("05");
+        page.formYearInput.setValue("2021");
+        page.purchaseButton.click();
+
+        sleep(2000);
+
+        page.confirmationBoxText.shouldHave(Condition.matchText("Amount: 790 USD"))
+                .shouldHave(Condition.matchText("Card Number: 21323 454356 456456"))
+                .shouldHave(Condition.matchText("Name: Pratik"));
+
+        page.confirmationBoxOkBtn.click();
+
+
     }
 }
